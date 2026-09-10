687a93a49727fe34b26aa0bf6ef0ef96f315faaa
Emmanuel Patrick <119854906+g0lgi@users.noreply.github.com>
2026-09-10 16:55:05 +0700

    Add: min-cost-to-connect-points - submission-2

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-2.py
@@ -0,0 +1,35 @@
+import heapq
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        # pq = []
+        # pq.append(0)
+
+        distances = [math.inf] * len(points)
+        distances[0] = 0
+        visited = [False] * len(points)
+
+        for _ in range(len(points)):
+            # print(distances)
+            curr = -1
+            for i in range(len(distances)):
+                if not visited[i] and (curr == -1 or distances[i] < distances[curr]):
+                    curr = i
+
+            visited[curr] = True
+
+            for j in range(len(points)):
+                if visited[j]:
+                    continue
+                curr_distance = abs(points[curr][0] - points[j][0]) + abs(points[curr][1] - points[j][1])
+                # print(curr_distance)
+                if curr_distance < distances[j]:
+                    distances[j] = curr_distance
+
+        # print(visited)
+        # print(distances)
+
+        result = 0
+        for k in distances:
+            result += k
+        
+        return result
